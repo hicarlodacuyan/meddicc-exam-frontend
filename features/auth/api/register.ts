3014b06eecbf6edc0af0ee4dde860df708ae67ea
Carlo Dacuyan <hi.carlodacuyan@gmail.com>
2025-03-29 09:12:26 +0800

    Add register auth feature

---
+++ features/auth/api/register.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { State } from "@/types/api";
+import apiClient from "@/lib/api-client";
+import { cookies } from "next/headers";
+
+export async function register(prevState: State, formData: FormData) {
+  const { username, email, password } = Object.fromEntries(formData);
+  const cookieStore = await cookies();
+  cookieStore.delete("accessToken");
+
+  try {
+    const response = await apiClient.post("/register/", {
+      username,
+      email,
+      password,
+    });
+  } catch (error: any) {
+    return {
+      message: "Failed to register.",
+      errors: error.response?.data || { general: ["An error occurred"] },
+    };
+  }
+
+  redirect("/login");
+}
